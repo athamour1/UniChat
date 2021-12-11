35b3a0df1db8b80abc74c243f017d1cbd3a82397
Thanos Mourtzoukos <thanosmour.tk@gmail.com>
2021-12-11 14:32:14 +0200

    Create: new component auditLogTable

--- src/pages/adminPanel.vue
+++ src/pages/adminPanel.vue
@@ -1,10 +1,45 @@
 <template>
   <q-page>
-    admin
+    <q-tabs
+      v-model="tab"
+      dense
+      class="text-white"
+      style="background-color: #3a4251"
+      active-color="white"
+      indicator-color="primary"
+      align="justify"
+    >
+      <q-tab name="1" label="Audit Log" no-caps />
+      <q-tab name="2" label="Create Users" no-caps />
+      <q-tab name="3" label="Edit Categories" no-caps />
+    </q-tabs>
+
+    <q-tab-panels v-model="tab" animated class="text-white" style="background-color: #424c5d">
+      <q-tab-panel name="1">
+      </q-tab-panel>
+
+      <q-tab-panel name="2">
+        <div class="text-h6">Alarms</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="3">
+        <div class="text-h6">Movies</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels>
   </q-page>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-}
+  setup() {
+    const tab = ref('1')
+    return {
+      tab
+    }
+  }
+};
 </script>
